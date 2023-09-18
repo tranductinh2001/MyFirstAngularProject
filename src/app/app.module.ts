04eb777d5b451b635b022dd9e5c9e283ff628eab
tranductinh2001 <0352911750a@gmail.com>
2023-09-18 20:50:42 +0700

    init angular1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StockComponent } from './stock/stock.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StockComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/stock/stock.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'src/app/model/stock';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.css']
+})
+
+export class StockComponent implements OnInit {
+
+  public stock: Stock;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite state for this stock', event);
+    this.stock.favorite = this.stock.favorite;
+  }
+}
+
+
